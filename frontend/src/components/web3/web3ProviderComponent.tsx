e18715e436c28aa67840af236a253dbbd9b75afa
marekakro <marekakro@users.noreply.github.com>
2018-06-28 11:49:00 +0200

    AKRO-54 Integrate frontend and backend

---
+++ frontend/src/components/web3/web3ProviderComponent.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import {connect, Dispatch} from "react-redux";
+
+import { fetchAccountAction, fetchNetworkAction } from "../../redux/actions/web3Action";
+
+export interface PropsFromDispatch {
+    fetchAccount: () => void;
+    fetchNetwork: () => void;
+}
+
+const ONE_SECOND = 1000;
+const ONE_MINUTE = ONE_SECOND * 60;
+let interval: any = null;
+let networkInterval: any = null;
+
+class Web3ProviderComponent extends React.Component<PropsFromDispatch, {}> {
+
+    public componentDidMount() {
+        this.props.fetchAccount();
+        this.props.fetchNetwork();
+        this.initPoll();
+        this.initNetworkPoll();
+    }
+
+    public componentWillUnmount() {
+        clearInterval(interval);
+        clearInterval(networkInterval);
+    }
+
+    public render() {
+        return null;
+    }
+
+    private initPoll() {
+        if (!interval) {
+            interval = setInterval(this.props.fetchAccount, ONE_MINUTE);
+        }
+    }
+
+    private initNetworkPoll() {
+        if (!networkInterval) {
+            networkInterval = setInterval(this.props.fetchNetwork, ONE_MINUTE);
+        }
+    }
+}
+
+function mapStateToProps() {
+    return {};
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+    return {
+        fetchAccount: () => dispatch(fetchAccountAction()),
+        fetchNetwork: () => dispatch(fetchNetworkAction()),
+    };
+}
+
+export default connect<{}, PropsFromDispatch, {}>(mapStateToProps, mapDispatchToProps)(Web3ProviderComponent);
