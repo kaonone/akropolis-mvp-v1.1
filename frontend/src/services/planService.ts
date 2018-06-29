82c655d13e326cf546c9ee6a8f017cd5fee30b17
wojciechakro <wojciechakro@users.noreply.github.com>
2018-06-29 18:38:34 +0200

    AKRO-58 Add a retirement calculator that does not return a mock value

--- frontend/src/services/planService.ts
+++
@@ -1,9 +0,0 @@
-import {PlanAfterCalculate, PlanValues} from "../models/Onboarding";
-
-export function calculatePlanValuesService(planValues: PlanValues): PlanAfterCalculate {
-    return {
-        needToSave: 777,
-        pensionValue: 15000,
-        projectReturns: 7,
-    };
-}
