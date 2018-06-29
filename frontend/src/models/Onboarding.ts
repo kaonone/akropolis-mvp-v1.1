fc2e698f953f6acd5345495f281a20d34cd80328
grzegorzakro <grzegorzakro@users.noreply.github.com>
2018-06-29 12:56:35 +0200

    AKRO-46 Fix init state in input fields

--- frontend/src/models/Onboarding.ts
+++ frontend/src/models/Onboarding.ts
@@ -1,9 +1,9 @@
 export interface PlanValues {
-    ageAtRetirement: number;
-    currentAge: number;
-    desiredAnnualIncome: number;
-    existingPension: number;
-    savingPerMonth: number;
+    ageAtRetirement: number | undefined;
+    currentAge: number | undefined; 
+    desiredAnnualIncome: number | undefined;
+    existingPension: number | undefined;
+    savingPerMonth: number | undefined;
 }
 
 export interface PlanAfterCalculate {
