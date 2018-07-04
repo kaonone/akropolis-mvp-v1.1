e8dcf314b42ea7813c873b5372a3aa4d0494cffb
wojciechakro <wojciechakro@users.noreply.github.com>
2018-07-04 19:28:11 +0200

    AKRO-90 Update product list screen to new requirements

--- frontend/src/models/Products.ts
+++ frontend/src/models/Products.ts
@@ -1,8 +1,37 @@
+export enum ProductRating {
+    CONSERVATIVE, BALANCED, AGGRESSIVE
+}
+
 export interface Product {
     fundDescription: string;
     fundName: string;
-    fundPercent: string;
-    fundRating: string;
-    fundValueDescription: string;
+    fundPastReturns: string;
+    fundReputation: string;
+    fundRiskRating: string;
     id: number;
+}
+
+function riskRatingToProductRating(risk: any) {
+    if (risk > 90) {
+        return ProductRating.AGGRESSIVE;
+    } else if (risk > 50) {
+        return ProductRating.BALANCED;
+    } else {
+        return ProductRating.CONSERVATIVE;
+    }
+}
+
+function reputationRatingToStars(reputation: any): string {
+    return "" + (Math.round((reputation / 20) * 10) / 10);
+}
+
+export function tupleToProduct(tuple: any): Product {
+    return {
+        fundDescription: tuple[6],
+        fundName: tuple[0],
+        fundPastReturns: tuple[4],
+        fundReputation: reputationRatingToStars(tuple[5]),
+        fundRiskRating: ProductRating[riskRatingToProductRating(tuple[3])],
+        id: tuple[7]
+    };
 }
--- frontend/src/redux/actions/myProductAction.ts
+++ frontend/src/redux/actions/myProductAction.ts
@@ -1,91 +1,93 @@
 // import axios from "axios";
+import {config} from "../../config/config";
 import * as constants from "../../constants/actions";
-import { Product } from "./../../models/Products";
+import {Product, tupleToProduct} from "./../../models/Products";
 import {Action} from "./action";
 
-const listOfProductsMockData =
-    [
-        {
-            fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-            fundName: "Fund name",
-            fundPercent: "12",
-            fundRating: "4.9",
-            fundValueDescription: "Conservative",
-            id: 0,
-        },
-        {
-            fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-            fundName: "Fund name",
-            fundPercent: "2",
-            fundRating: "4.9",
-            fundValueDescription: "Conservative",
-            id: 1,
-        },
-        {
-            fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-            fundName: "Fund name",
-            fundPercent: "9",
-            fundRating: "4.9",
-            fundValueDescription: "Conservative",
-            id: 2,
-        },
-        {
-            fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-            fundName: "Fund name",
-            fundPercent: "6",
-            fundRating: "4.9",
-            fundValueDescription: "Conservative",
-            id: 3,
-        },
-        {
-            fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-            fundName: "Fund name",
-            fundPercent: "9",
-            fundRating: "4.9",
-            fundValueDescription: "Conservative",
-            id: 4,
-        },
-        {
-            fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-            fundName: "Fund name",
-            fundPercent: "20",
-            fundRating: "4.9",
-            fundValueDescription: "Conservative",
-            id: 5,
-        },
-        {
-            fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-            fundName: "Fund name",
-            fundPercent: "23",
-            fundRating: "4.9",
-            fundValueDescription: "Conservative",
-            id: 6,
-        },
-        {
-            fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-            fundName: "Fund name",
-            fundPercent: "5",
-            fundRating: "4.9",
-            fundValueDescription: "Conservative",
-            id: 7,
-        }
-    ];
+const listOfProductsMockData = (config.deployment.deployedFunds).map(tupleToProduct);
+// const listOfProductsMockData =
+//     [
+//         {
+//             fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+//             when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+//             fundName: "Fund name",
+//             fundPastReturns: "12",
+//             fundReputation: "4.9",
+//             fundRiskRating: "Conservative",
+//             id: 0,
+//         },
+//         {
+//             fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+//             when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+//             fundName: "Fund name",
+//             fundPastReturns: "2",
+//             fundReputation: "4.9",
+//             fundRiskRating: "Conservative",
+//             id: 1,
+//         },
+//         {
+//             fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+//             when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+//             fundName: "Fund name",
+//             fundPastReturns: "9",
+//             fundReputation: "4.9",
+//             fundRiskRating: "Conservative",
+//             id: 2,
+//         },
+//         {
+//             fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+//             when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+//             fundName: "Fund name",
+//             fundPastReturns: "6",
+//             fundReputation: "4.9",
+//             fundRiskRating: "Conservative",
+//             id: 3,
+//         },
+//         {
+//             fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+//             when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+//             fundName: "Fund name",
+//             fundPastReturns: "9",
+//             fundReputation: "4.9",
+//             fundRiskRating: "Conservative",
+//             id: 4,
+//         },
+//         {
+//             fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+//             when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+//             fundName: "Fund name",
+//             fundPastReturns: "20",
+//             fundReputation: "4.9",
+//             fundRiskRating: "Conservative",
+//             id: 5,
+//         },
+//         {
+//             fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+//             when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+//             fundName: "Fund name",
+//             fundPastReturns: "23",
+//             fundReputation: "4.9",
+//             fundRiskRating: "Conservative",
+//             id: 6,
+//         },
+//         {
+//             fundDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+//             when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+//             fundName: "Fund name",
+//             fundPastReturns: "5",
+//             fundReputation: "4.9",
+//             fundRiskRating: "Conservative",
+//             id: 7,
+//         }
+//     ];
 
 export function fetchProductsDataAction(): Action<constants.FETCH_PRODUCTS_DATA, Product[]> {
     return {
