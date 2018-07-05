08a6c90846c85569f151ef5592d323b9453c2087
marekakro <marekakro@users.noreply.github.com>
2018-07-05 15:49:17 +0200

    AKRO-96 Switch web app routing to use bookmarks

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import { addLocaleData, IntlProvider } from "react-intl";
 import { Provider } from "react-redux";
-import { BrowserRouter, Switch } from "react-router-dom";
+import { HashRouter, Switch } from "react-router-dom";
 
 import * as en from "react-intl/locale-data/en";
 
@@ -21,11 +21,11 @@ addLocaleData([...en]);
 ReactDOM.render(
     <IntlProvider locale={selectedLocale} messages={messages[selectedLocale]}>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <Switch>
                     <LayoutWrapper/>
                 </Switch>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </IntlProvider>,
     document.getElementById("app") as HTMLElement,
