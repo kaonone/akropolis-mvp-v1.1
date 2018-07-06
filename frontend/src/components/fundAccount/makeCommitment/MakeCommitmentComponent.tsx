efb4521f60163c88a6f6b21a4cfeb95cf792cc52
wojciechakro <wojciechakro@users.noreply.github.com>
2018-07-06 11:45:13 +0200

    AKRO-103 Make Commitment - eth payment slider increments

--- frontend/src/components/fundAccount/makeCommitment/MakeCommitmentComponent.tsx
+++ frontend/src/components/fundAccount/makeCommitment/MakeCommitmentComponent.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { FormattedMessage } from "react-intl";
+import {FormattedMessage} from "react-intl";
 import SubNavigationComponent from "../../../components/subNavigation/SubNavigationComponent";
 // import SubNavigationComponent from "../../components/subNavigation/SubNavigationComponent";
 import InputRange from "../../inputRange/InputRangeComponent";
 
-import { StepOne } from "../../../views/fundAccount/FundAccountView";
+import {StepOne} from "../../../views/fundAccount/FundAccountView";
 
 import "./c-make-commitment.css";
 
@@ -54,33 +54,39 @@ export default class MakeCommitmentComponent extends React.Component<Props, Stat
         return (
             <>
                 <FormattedMessage id="fundAccount.makeCommitment">{
-                    (makeCommitment: string) => <SubNavigationComponent title={makeCommitment} spaceForArrow={false} />}
+                    (makeCommitment: string) => <SubNavigationComponent title={makeCommitment} spaceForArrow={false}/>}
                 </FormattedMessage>
                 <h3 className="c-make-commitment__headline">
-                    <FormattedMessage id="fundAccount.makeCommitmentToSaving" />
+                    <FormattedMessage id="fundAccount.makeCommitmentToSaving"/>
                 </h3>
                 <div className="c-make-commitment__wrapper-section">
-                    <label className="c-make-commitment__label-range"><FormattedMessage id="fundAccount.iWillMakeARegularPaymentOf" /></label>
-                    <InputRange symbol="ETH" value={rangeEth} max={ETHBalance} min={0}
-                        onChange={this.handleRangeChange("rangeEth")} />
+                    <label className="c-make-commitment__label-range"><FormattedMessage
+                        id="fundAccount.iWillMakeARegularPaymentOf"/></label>
+                    <InputRange symbol="ETH" value={rangeEth} max={ETHBalance} min={0} step={0.01}
+                                onChange={this.handleRangeChange("rangeEth")}/>
                 </div>
                 <div className="c-make-commitment__wrapper-section">
-                    <label className="c-make-commitment__label-range"><FormattedMessage id="fundAccount.every" /></label>
+                    <label className="c-make-commitment__label-range"><FormattedMessage id="fundAccount.every"/></label>
                     <div className="c-make-commitment__wrapper-period">
-                        <button className={`c-make-commitment__btn-period ${period === "week" && "c-make-commitment__btn-period--active"}`}
-                                onClick={this.setPeriod("week")}><FormattedMessage id="fundAccount.week" /></button>
-                        <button className={`c-make-commitment__btn-period ${period  === "month" && "c-make-commitment__btn-period--active"}`}
-                                onClick={this.setPeriod("month")}><FormattedMessage id="fundAccount.month" /></button>
-                        <button className={`c-make-commitment__btn-period ${period  === "quarter" && "c-make-commitment__btn-period--active"}`}
-                                onClick={this.setPeriod("quarter")}><FormattedMessage id="fundAccount.quarter" /></button>
+                        <button
+                            className={`c-make-commitment__btn-period ${period === "week" && "c-make-commitment__btn-period--active"}`}
+                            onClick={this.setPeriod("week")}><FormattedMessage id="fundAccount.week"/></button>
+                        <button
+                            className={`c-make-commitment__btn-period ${period === "month" && "c-make-commitment__btn-period--active"}`}
+                            onClick={this.setPeriod("month")}><FormattedMessage id="fundAccount.month"/></button>
+                        <button
+                            className={`c-make-commitment__btn-period ${period === "quarter" && "c-make-commitment__btn-period--active"}`}
+                            onClick={this.setPeriod("quarter")}><FormattedMessage id="fundAccount.quarter"/></button>
                     </div>
                 </div>
                 <div className="c-make-commitment__wrapper-section">
-                    <label className="c-make-commitment__label-range"><FormattedMessage id="fundAccount.forThePeriodOf"/></label>
+                    <label className="c-make-commitment__label-range"><FormattedMessage
+                        id="fundAccount.forThePeriodOf"/></label>
                     <InputRange symbol="YEARS" value={years} max={100} min={0}
-                        onChange={this.handleRangeChange("years")} />
+                                onChange={this.handleRangeChange("years")}/>
                 </div>
-                <p className="c-make-commitment__description"><FormattedMessage id="fundAccount.longerCommitmentsMeanMoreRewards" /></p>
+                <p className="c-make-commitment__description"><FormattedMessage
+                    id="fundAccount.longerCommitmentsMeanMoreRewards"/></p>
                 <button className="o-btn o-btn--wide"
                         onClick={() => this.props.onConfirm(this.state.form)} disabled={isNotValid}>
                     <FormattedMessage id="fundAccount.makeCommitment"/>
@@ -91,7 +97,7 @@ export default class MakeCommitmentComponent extends React.Component<Props, Stat
 
     private setPeriod = (period: "week" | "month" | "quarter") => {
         return () => {
-            const form = { ...this.state.form };
+            const form = {...this.state.form};
             form.period = period;
             this.setState({
                 ...this.state,
@@ -102,7 +108,7 @@ export default class MakeCommitmentComponent extends React.Component<Props, Stat
 
     private handleRangeChange(field: string) {
         return (value: number) => {
-            const form = { ...this.state.form };
+            const form = {...this.state.form};
             form[field] = value;
             this.setState({
                 ...this.state,
